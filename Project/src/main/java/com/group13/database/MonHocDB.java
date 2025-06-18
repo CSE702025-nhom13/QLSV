4e6850849d3f709e2ad1e9499ebdb31cdcd5cc2b
dahquang16 <23010230@st.phenikaa-uni.edu.vn>
2025-06-18 10:47:11 +0700

    Update MonHocDB.java

--- Project/src/main/java/com/group13/database/MonHocDB.java
+++ Project/src/main/java/com/group13/database/MonHocDB.java
@@ -1,47 +1,27 @@
 package com.group13.database;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import com.group13.model.Diem;
+import com.group13.model.MonHoc;
 
-public class DiemDB {
+public class MonHocDB {
+    public static List<MonHoc> monHocs = new ArrayList<>();
 
-    public static List<Diem> diemList = new ArrayList<>();
-
-    public DiemDB() {
-    }
-
-    public static void addDiem(Diem diem) {
-        diemList.add(diem);
-    }
-
-    public static Diem searchDiem(String maSV, String maMH) {
-
-        for (Diem diem : diemList) {
-            if (diem.getMaSV().equals(maSV) && diem.getMaMH().equals(maMH)) {
-                return diem;
+    public static MonHoc searchMonHoc(String maMH) {
+        for (MonHoc monHoc : monHocs) {
+            if (monHoc.getMaMH().equals(maMH)) {
+                return monHoc;
             }
         }
         return null;
     }
 
-    public static boolean updateDiem(Diem updatedDiem) {
-        for (int i = 0; i < diemList.size(); i++) {
-            Diem diem = diemList.get(i);
-            if (diem.getMaSV().equals(updatedDiem.getMaSV()) && diem.getMaMH().equals(updatedDiem.getMaMH())) {
-                diemList.set(i, updatedDiem);
-                return true;
+    public static void updateMonHoc(MonHoc monHoc) {
+        for (MonHoc mHoc2 : monHocs) {
+            if (mHoc2.getMaMH().equals(monHoc.getMaMH())) {
+                mHoc2.setTenMH(monHoc.getTenMH());
+                break;
             }
         }
-        return false;
-    }
-
-    public static boolean removeDiem(String maSV, String maMH) {
-        return diemList.removeIf(diem -> diem.getMaSV().equals(maSV) && diem.getMaMH().equals(maMH));
-    }
-
-    public static List<Diem> getAllDiems() {
-        return new ArrayList<>(diemList);
     }
 }
